ab5e5a891ba0bf245575fedcaf0bf23f4299dd60
Maxime Papin <max.papin33@gmail.com>
2020-01-30 11:05:17 +0100

    programme page created

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Act;
 use App\Entity\Event;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,6 +22,17 @@ class WildController extends AbstractController
         $events = $em->getRepository(Event::class)->findAll();
         return $this->render('wild/showEvents.html.twig', [
             'events' => $events,
+        ]);
+    }
+
+    /**
+     * @Route("/programme", name="_programme")
+     */
+    public function showActs(EntityManagerInterface $em)
+    {
+        $acts = $em->getRepository(Act::class)->findAll();
+        return $this->render('wild/showActs.html.twig', [
+            'acts' => $acts,
         ]);
     }
 }
